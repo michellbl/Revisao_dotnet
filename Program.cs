053a02de7dc6aab35fdb105773369e2eca976a1d
Michell Bezerra de Lima <michellblima@gmail.com>
2020-02-10 22:14:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno[] alunos = new Aluno[5]; 
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArithmeticException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        
+                        break;
+                    case "2":
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} . NOTA: {a.Nota}");
+                            }
+                            
+                        }
+                        break;
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+                        for (int i=0; i < alunos.Length; i++)
+                        
+                        if (!string.IsNullOrEmpty(alunos[i].Nome))
+                        {
+                            notaTotal = notaTotal + alunos[i].Nota;
+                        }
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+
+                Console.WriteLine("Informe a opção desejada:");
+                Console.WriteLine("1 - Inserir nome de aluno");
+                Console.WriteLine("2 - Listar alunos:");
+                Console.WriteLine("3 - Calcular média geral:");
+                Console.WriteLine("X - Sair");
+                Console.WriteLine();
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir nome de aluno");
+            Console.WriteLine("2 - Listar alunos:");
+            Console.WriteLine("3 - Calcular média geral:");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
